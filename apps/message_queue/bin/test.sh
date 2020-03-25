d29c056a5edbed445059b2d2cfdd24d42c487bff
Omar Ayad <omarayad1@gmail.com>
2020-03-25 13:12:19 +0100

    added become user

--- apps/message_queue/bin/test.sh
+++ apps/message_queue/bin/test.sh
@@ -3,4 +3,4 @@
 set -ex
 
 # should run molecule, a dry run is fine for now
-ansible-playbook -i local/hosts.ini install.yml --check -vv
+ansible-playbook -i local/hosts.ini install.yml --become-user root --check -vv
