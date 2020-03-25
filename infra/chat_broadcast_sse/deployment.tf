765a20bfdb4324a0cbf1abe28fa6b48407ee2d98
Omar Ayad <omarayad1@gmail.com>
2020-03-25 11:37:46 +0100

    fix cmd

---
+++ infra/chat_broadcast_sse/deployment.tf
@@ -0,0 +1,47 @@
+resource "kubernetes_deployment" "chat_broadcast_sse" {
+  metadata {
+    name = "chat-broadcast-sse"
+
+    labels = {
+      app  = "chat_broadcast_sse"
+      role = "broadcast"
+      tier = "backend"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app  = "chat_broadcast_sse"
+        role = "broadcast"
+        tier = "backend"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app  = "chat_broadcast_sse"
+          role = "broadcast"
+          tier = "backend"
+        }
+      }
+      spec {
+        container {
+          image             = "chat_broadcast_sse:latest"
+          name              = "chat-broadcast-sse"
+          image_pull_policy = "Never"
+
+          resources {
+            requests {
+              cpu    = "100m"
+              memory = "100Mi"
+            }
+          }
+        }
+      }
+    }
+  }
+}
